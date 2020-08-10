7a809b91fabae07c68069daf50b07699d440ecf8
kvosarkoceles <sarkoceles@ciencias.unam.mx>
2020-08-10 12:36:36 -0500

    editando  nosotros agragando cabecera

--- application/views/web/nosotros.php
+++ application/views/web/nosotros.php
@@ -1,3 +1,14 @@
+	<!-- Home -->
+
+	<div class="home">
+		<div class="home_background_container prlx_parent">
+			<div class="home_background prlx" style="background-image:url(<?php echo base_url();?>assets/images/courses_background.jpg)"></div>
+		</div>
+		<div class="home_content">
+			<h1>Nosotros</h1>
+		</div>
+	</div>
+
 <!-- Services -->
 
 <div class="services page_section">
