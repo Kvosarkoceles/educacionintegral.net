5cb9198bfd167c817e1b35a047bb12199a4ebecc
Luigui9393 <aztlan9393@gmail.com>
2020-08-12 14:41:17 -0500

    Update profesores.php
    Cambio de titulo y algunos textos

--- application/views/web/profesores.php
+++ application/views/web/profesores.php
@@ -5,7 +5,7 @@
 			<div class="home_background prlx" style="background-image:url(<?php echo base_url();?>assets/images/teachers_background.jpg)"></div>
 		</div>
 		<div class="home_content">
-			<h1>Teachers</h1>
+			<h1>Profesores</h1>
 		</div>
 	</div>
 
@@ -24,7 +24,7 @@
 						</div>
 						<div class="card-body text-center">
 							<div class="card-title"><a href="#">Maria Smith</a></div>
-							<div class="card-text">Graphic Designer</div>
+							<div class="card-text">Diseñadora gráfica</div>
 							<div class="teacher_social">
 								<ul class="menu_social">
 									<li class="menu_social_item"><a href="#"><i class="fab fa-pinterest"></i></a></li>
@@ -47,7 +47,7 @@
 						</div>
 						<div class="card-body text-center">
 							<div class="card-title"><a href="#">Christian Blue</a></div>
-							<div class="card-text">Graphic Designer</div>
+							<div class="card-text">Diseñador gráfico</div>
 							<div class="teacher_social">
 								<ul class="menu_social">
 									<li class="menu_social_item"><a href="#"><i class="fab fa-pinterest"></i></a></li>
@@ -70,7 +70,7 @@
 						</div>
 						<div class="card-body text-center">
 							<div class="card-title"><a href="#">James Brown</a></div>
-							<div class="card-text">Graphic Designer</div>
+							<div class="card-text">Diseñador gráfico</div>
 							<div class="teacher_social">
 								<ul class="menu_social">
 									<li class="menu_social_item"><a href="#"><i class="fab fa-pinterest"></i></a></li>
@@ -93,7 +93,7 @@
 						</div>
 						<div class="card-body text-center">
 							<div class="card-title"><a href="#">Julie Denver</a></div>
-							<div class="card-text">Graphic Designer</div>
+							<div class="card-text">Diseñadora gráfica</div>
 							<div class="teacher_social">
 								<ul class="menu_social">
 									<li class="menu_social_item"><a href="#"><i class="fab fa-pinterest"></i></a></li>
@@ -116,7 +116,7 @@
 						</div>
 						<div class="card-body text-center">
 							<div class="card-title"><a href="#">Julie Denver</a></div>
-							<div class="card-text">Graphic Designer</div>
+							<div class="card-text">Diseñadora gráfica</div>
 							<div class="teacher_social">
 								<ul class="menu_social">
 									<li class="menu_social_item"><a href="#"><i class="fab fa-pinterest"></i></a></li>
@@ -139,7 +139,7 @@
 						</div>
 						<div class="card-body text-center">
 							<div class="card-title"><a href="#">Julie Denver</a></div>
-							<div class="card-text">Graphic Designer</div>
+							<div class="card-text">Diseñadora gráfica</div>
 							<div class="teacher_social">
 								<ul class="menu_social">
 									<li class="menu_social_item"><a href="#"><i class="fab fa-pinterest"></i></a></li>
