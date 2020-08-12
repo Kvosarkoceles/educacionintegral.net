d4dc2c0a46fa62f21a4b62bb0aea7fce8647e836
kvosarkoceles <sarkoceles@ciencias.unam.mx>
2020-08-12 17:01:12 -0500

    pagina nosotros

--- application/views/web/nosotros.php
+++ application/views/web/nosotros.php
@@ -17,7 +17,7 @@
 			<div class="row">
 				<div class="col">
 					<div class="section_title text-center">
-						<h1>Nuestros servicios</h1>
+						<h1>¿Quienes somos?</h1>
 					</div>
 				</div>
 			</div>
@@ -52,68 +52,12 @@
 				
 			</div>
 
-			<div class="row services_row">
-
-				<div class="col-lg-4 service_item text-left d-flex flex-column align-items-start justify-content-start">
-					<div class="icon_container d-flex flex-column justify-content-end">
-						<img src="<?php echo base_url();?>assets/images/earth-globe.svg" alt="">
-					</div>
-					<h3>Cursos en línea</h3>
-					<p>Aquí va texto informativo, o datos que se quieran proporcionar, o en su defecto especificaciones, o ventas de este curso, también testimonios personales como características generales de lo que se quiere ofrecer, es texto simple y corto.</p>
-				</div>
-
-				<div class="col-lg-4 service_item text-left d-flex flex-column align-items-start justify-content-start">
-					<div class="icon_container d-flex flex-column justify-content-end">
-						<img src="<?php echo base_url();?>assets/images/exam.svg" alt="">
-					</div>
-					<h3>Cursos de interior</h3>
-					<p>Aquí va texto informativo, o datos que se quieran proporcionar, o en su defecto especificaciones, o ventas de este curso, también testimonios personales como características generales de lo que se quiere ofrecer, es texto simple y corto.</p>
-				</div>
-
-				<div class="col-lg-4 service_item text-left d-flex flex-column align-items-start justify-content-start">
-					<div class="icon_container d-flex flex-column justify-content-end">
-						<img src="<?php echo base_url();?>assets/images/books.svg" alt="">
-					</div>
-					<h3>Biblioteca asombrosa</h3>
-					<p>Aquí va texto informativo, o datos que se quieran proporcionar, o en su defecto especificaciones, o ventas de este curso, también testimonios personales como características generales de lo que se quiere ofrecer, es texto simple y corto.</p>
-				</div>
-
-				<div class="col-lg-4 service_item text-left d-flex flex-column align-items-start justify-content-start">
-					<div class="icon_container d-flex flex-column justify-content-end">
-						<img src="<?php echo base_url();?>assets/images/professor.svg" alt="">
-					</div>
-					<h3>Profesores excepcionales</h3>
-					<p>Aquí va texto informativo, o datos que se quieran proporcionar, o en su defecto especificaciones, o ventas de este curso, también testimonios personales como características generales de lo que se quiere ofrecer, es texto simple y corto.</p>
-				</div>
-
-				<div class="col-lg-4 service_item text-left d-flex flex-column align-items-start justify-content-start">
-					<div class="icon_container d-flex flex-column justify-content-end">
-						<img src="<?php echo base_url();?>assets/images/blackboard.svg" alt="">
-					</div>
-					<h3>Programas principales</h3>
-					<p>Aquí va texto informativo, o datos que se quieran proporcionar, o en su defecto especificaciones, o ventas de este curso, también testimonios personales como características generales de lo que se quiere ofrecer, es texto simple y corto.</p>
-				</div>
-
-				<div class="col-lg-4 service_item text-left d-flex flex-column align-items-start justify-content-start">
-					<div class="icon_container d-flex flex-column justify-content-end">
-						<img src="<?php echo base_url();?>assets/images/mortarboard.svg" alt="">
-					</div>
-					<h3>Diploma de graduación</h3>
-					<p>Aquí va texto informativo, o datos que se quieran proporcionar, o en su defecto especificaciones, o ventas de este curso, también testimonios personales como características generales de lo que se quiere ofrecer, es texto simple y corto.</p>
-				</div>
-
-			</div>
+			
 		</div>
 	</div>
 
 
-
-
-
-<!-- Popular -->
-
-<div class="popular page_section">
-		<div class="container">
+	<div class="container">
 			<div class="row">
 				<div class="col">
 					<div class="section_title text-center">
@@ -147,7 +91,7 @@
 					<div class="card">
 						<img class="card-img-top" src="<?php echo base_url();?>assets/images/course_2.jpg" alt="https://unsplash.com/@cikstefan">
 						<div class="card-body text-center">
-							<div class="card-title"><a href="courses.html">Guía para principiantes de HTML</a></div>
+							<div class="card-title"><a href="courses.html">Guía para HTML</a></div>
 							<div class="card-text">Guía PDF, Capa, Objetos inteligentes, etc...</div>
 						</div>
 						<div class="price_box d-flex flex-row align-items-center">
@@ -179,4 +123,10 @@
 				</div>
 			</div>
 		</div>
+
+
+<!-- Popular -->
+
+<div class="popular page_section">
+		
 	</div>
